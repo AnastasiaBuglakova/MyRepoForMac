bd9d309ea7fa769b9c577d3ba13a6788ff8f2fb6
Anastasia Buglakova <bugla2005@mail.ru>
2022-12-04 11:41:08 +0300

    lesson8 done on mac

---
+++ ListMaster/ArrayMaster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace ListMaster
+{
+	public class ArrayMaster
+	{
+			int[] storage = new int[0];
+			int count = 0;
+
+			public void Add(int item)
+			{
+				Array.Resize(ref storage, count + 1);
+				storage[count++] = item;
+			}
+			public int GetValue(int index)
+			{
+				return storage[index];
+
+			}
+		public int GetCount()
+		{
+			return count;
+		}
+	}
+}
+
